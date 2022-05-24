91781cc25d4c7646180bf2f37dd858687629079e
vivek-Reddy333 <vivekgarlapti15103@gmail.com>
2022-05-24 08:37:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from './Page/Dashboard'
+
+function App() {
+  return (
+    <div className="container">
+        <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
